24c7545d0c6d684a36cd0be98fac873c8e12d063
vinncentsc <136545740+vinncentsc@users.noreply.github.com>
2024-01-14 13:16:20 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const burgerMenu = document.querySelector('.burger-menu');
-const menu = document.querySelector('.menu');
-
-burgerMenu.addEventListener('click', () => {
-  burgerMenu.classList.toggle('active');
-});
